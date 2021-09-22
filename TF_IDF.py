a3d98639722b67bdd4eeb026abc9168207c5cfc1
John Stevenson <john.stevenson@wsu.edu>
2021-09-21 19:33:00 -0700

    Initial creation of TF_IDF class

---
+++ TF_IDF.py
@@ -0,0 +1,14 @@
+class TF_IDF(object):
+    """TF-IDF Class"""
+    
+    # Constructor for TF_IDF class
+    def __init__(self, dataFile):
+        # Make index over terms in the corpus
+
+    # Returns a list of k tuples consisting of 
+    # a document's id and its tfidf score for 
+    # the given query Q. Performs tfidf operation
+    # on the corpus given to the instance called 
+    # upon creation
+    def tfidf(self, Q, k): 
+        # Perform
